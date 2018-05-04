06897d00e64eb978584f9fd5ed513571c8935f06
Matthew Dyet <matthew@diomades.com>
2018-05-04 16:56:07 +0800

    Bakery Scene Start
    Initial movement, placeholder sprite swapping, Enums set up

---
+++ Assets/Scripts/CharSprites.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CharState { Idle, Moving, Pie, Cake };
+
+public class CharSprites : MonoBehaviour {
+    public Sprite idleSprite1;
+    public Sprite idleSprite2;
+    public Sprite walkSprite;
+    public Sprite bakingSprite1;
+    public Sprite bakingSprite2;
+
+    private CharState _curState;
+    private string _lastSprite;
+
+    private bool _doAnimate;
+
+    public void ChangeState(CharState state)
+    {
+        //Set the new state
+        _curState = state;
+        //Stop any current animation
+        StopCoroutine(Animate());
+
+        //Change the sprite
+        ChangeSprite();
+    }
+
+    IEnumerator Animate()
+    {
+        yield return new WaitForSeconds(0.7f);
+        ChangeSprite();
+    }
+
+    private void ChangeSprite()
+    {
+        switch (_curState)
+        {
+            case CharState.Idle:
+                if (_lastSprite == idleSprite1.name)
+                {
+                    this.GetComponent<SpriteRenderer>().sprite = idleSprite2;
+                }
+                else
+                {
+                    this.GetComponent<SpriteRenderer>().sprite = idleSprite1;
+                }
+                StartCoroutine(Animate());
+                break;
+            case CharState.Moving:
+                this.GetComponent<SpriteRenderer>().sprite = walkSprite;
+                break;
+            case CharState.Pie:
+            case CharState.Cake:
+                if (_lastSprite == bakingSprite1.name)
+                {
+                    this.GetComponent<SpriteRenderer>().sprite = bakingSprite2;
+                }
+                else
+                {
+                    this.GetComponent<SpriteRenderer>().sprite = bakingSprite1;
+                }
+                StartCoroutine(Animate());
+                break;
+        }
+
+        //Set the name of this sprite as the last sprite and return it
+        _lastSprite = this.GetComponent<SpriteRenderer>().sprite.name;
+        Debug.Log(_lastSprite);
+    }
+}
+
+
---
+++ Assets/Scripts/MainLoop.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainLoop : MonoBehaviour {
+    public GameObject Char;
+    public Transform IdlePos;
+    public Transform CakePos;
+    public Transform PiePos;
+
+    public CharSprites charSprites;
+
+    private CharState lastState = CharState.Idle;
+    private CharState curState = CharState.Idle;
+
+    // Use this for initialization
+    //Replace Start later with a function called from cutscenes ending
+    void Start()
+    {
+        StartGameplay();
+    }
+
+    void Update()
+    {
+        if (curState != CharState.Moving)
+        {
+            if (Input.GetKeyUp("a") || Input.GetKeyUp("left"))
+            {
+                //Move to Cake State
+                if (curState == CharState.Idle)
+                {
+                    //SetState(CharState.Cake);
+                    StartCoroutine(MoveTo(CharState.Cake));
+                }
+                //Move to Idle State
+                else if (curState == CharState.Pie)
+                {
+                    //SetState(CharState.Idle);
+                    StartCoroutine(MoveTo(CharState.Idle));
+                }
+                //Being in Cake State does nothing
+            }
+            if (Input.GetKeyUp("d") || Input.GetKeyUp("right"))
+            {
+                //Move to Pie State
+                if (curState == CharState.Idle)
+                {
+                    //SetState(CharState.Pie);
+                    StartCoroutine(MoveTo(CharState.Pie));
+                }
+                //Move to Idle State
+                else if (curState == CharState.Cake)
+                {
+                    //SetState(CharState.Idle);
+                    StartCoroutine(MoveTo(CharState.Idle));
+                }
+                //Being in Pie State does nothing
+            }
+        }        
+    }
+
+    public void StartGameplay()
+    {
+        Char.transform.position = IdlePos.position;
+        charSprites.ChangeState(CharState.Idle);
+    }
+
+    IEnumerator MoveTo(CharState state)
+    {
+        //Store our last state, note that we're moving
+        lastState = curState;
+        curState = CharState.Moving;
+
+        Vector3 midPos = new Vector3();
+        Vector3 endPos = new Vector3();
+
+        //Figure out our mid position by checking it against our current position and next position
+        switch (state)
+        {
+            case CharState.Idle:
+                endPos = IdlePos.position;
+                midPos = Vector3.Lerp(Char.transform.position, IdlePos.position, 0.5f);
+                break;
+            case CharState.Pie:
+                endPos = PiePos.position;
+                midPos = Vector3.Lerp(Char.transform.position, PiePos.position, 0.5f);
+                break;
+            case CharState.Cake:
+                endPos = CakePos.position;
+                midPos = Vector3.Lerp(Char.transform.position, CakePos.position, 0.5f);
+                break;
+        }
+
+        //Calculations are done, wait a little before our first movement
+        yield return new WaitForSeconds(0.3f);
+
+        //Change our sprite for the Movement state
+        charSprites.ChangeState(curState);
+
+        //If the MidPos is less or more than our current position, rotate the facing direction of the mid frame
+        if (midPos.x < Char.transform.position.x)
+        {
+            //Face left
+            Char.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        //Move to mid point
+        Char.transform.position = midPos;
+
+        //Wait again
+        yield return new WaitForSeconds(1f);
+
+        Char.transform.position = endPos;
+
+        //Fix our state to the actual end state, and flip us back (if we moved left)
+        curState = state;
+        Char.GetComponent<SpriteRenderer>().flipX = false;
+        charSprites.ChangeState(curState);
+    }
+
+    public CharState CurrentState()
+    {
+        return curState;
+    }
+}
